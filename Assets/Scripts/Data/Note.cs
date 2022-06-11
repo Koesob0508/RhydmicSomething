325a0a50ebda014133b4b3fdf9f7201516dccec1
sittan2 <hang5050@naver.com>
2022-06-12 08:46:00 +0900

    Data 스크립트 생성

---
+++ Assets/Scripts/Data/Note.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public class Note
+{
+    public Define.eNoteType noteType;
+}
---
+++ Assets/Scripts/UI/UISheet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISheet : MonoBehaviour
+{
+    SheetBuilder SheetBuilder;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
