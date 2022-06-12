0b62fbec39282c6c4befabfcb6beddf8439e782a
Koesob3706 <epi3706@khu.ac.kr>
2022-06-12 13:50:57 +0900

    애니메이션

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : Character
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+    public float rotateSpeed = 180f;
+    private TestPlayerController playerController;
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+    private Vector3 moveDirection;
+
     void Start()
     {
-        
+        playerController = GetComponent<TestPlayerController>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+
+        playerAnimator.SetFloat("Move", moveDirection.magnitude);
+    }
+
+    new private void Move()
+    {
+        moveDirection = (Vector3.forward * playerController.verticalMove) + (Vector3.right * playerController.horizontalMove);
+        Vector3 moveDistance = moveDirection.normalized * moveSpeed * Time.deltaTime;
+        if (!(playerController.horizontalMove == 0 & playerController.verticalMove == 0))
+        {
+            playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * rotateSpeed);
+        }
     }
 }
---
+++ Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : PlayerController
+{
+    public string moveAxisName = "Vertical";
+    public string rotateAxisName = "Horizontal";
+    // 임시 방편, 왼쪽 ctrl, 마우스 왼쪽 버튼
+    public string attackButtonName = "Fire1";
+
+    public float verticalMove { get; private set; }
+    public float horizontalMove { get; private set; }
+    public bool attack { get; private set; }
+
+    void Update()
+    {
+        verticalMove = Input.GetAxis(moveAxisName);
+        horizontalMove = Input.GetAxis(rotateAxisName);
+        attack = Input.GetButton(attackButtonName);
+    }
+}
