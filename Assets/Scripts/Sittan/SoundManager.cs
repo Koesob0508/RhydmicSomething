cb7f8c98bb6d36ebaa7bba5125771b0a9392a6cd
sittan2 <49196800+sittan2@users.noreply.github.com>
2022-07-05 18:07:19 +0900

    Managers 및 하위 매니저들 생성

---
+++ Assets/Scripts/Sittan/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Prototype03
+{
+    public class SoundManager
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
